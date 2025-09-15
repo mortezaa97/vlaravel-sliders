ffbea613d73c5f75b75f2facc25d0ad66bb8ee11
mortezaa <mortezajafari76@gmail.com>
2025-09-15 17:26:16 +0330

    bugfix: namespace issue

--- src/Filament/Resources/Sliders/Pages/CreateSlider.php
+++ src/Filament/Resources/Sliders/Pages/CreateSlider.php
@@ -2,8 +2,9 @@
 
 namespace Mortezaa97\Sliders\Filament\Resources\Sliders\Pages;
 
-use App\Filament\Resources\Sliders\SliderResource;
+
 use Filament\Resources\Pages\CreateRecord;
+use Mortezaa97\Sliders\Filament\Resources\Sliders\SliderResource;
 
 class CreateSlider extends CreateRecord
 {
