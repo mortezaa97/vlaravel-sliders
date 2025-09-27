0fbd68b9aa1703e20fb75edda2aa15ddc6bfda42
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:38 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use Mortezaa97\Sliders\Http\Controllers\SlideController;
 use Mortezaa97\Sliders\Http\Controllers\SliderController;
 use Mortezaa97\Stories\Http\Controllers\StoryController;
 
-Route::prefix('api/sliders')->group(function () {
+Route::prefix('api/sliders')->middleware('api')->group(function () {
     Route::get('/', [SliderController::class, 'index'])->name('sliders.index');
     Route::get('/{slider}', [SliderController::class, 'show'])->name('sliders.show');
 });
-Route::prefix('api/slides')->group(function () {
+Route::prefix('api/slides')->middleware('api')->group(function () {
     Route::get('/', [SlideController::class, 'index'])->name('slides.index');
     Route::get('/{slide}', [SlideController::class, 'show'])->name('slides.show');
 });
